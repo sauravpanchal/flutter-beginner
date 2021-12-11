db4dc842fa3f50612b21bc013e7fa392247360ca
Saurav Panchal <61927693+sauravpanchal@users.noreply.github.com>
2021-12-12 00:38:09 +0530

    Create v_6.dart

---
+++ v_6.dart
@@ -0,0 +1,35 @@
+// pubspec.yaml
+fonts:
+   - family: Montserrat
+     fonts:
+       - asset: fonts/Montserrat/Montserrat-Regular.ttf
+
+         
+// main.dart
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My First App"),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text("Hello Learners !",
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color: Colors.grey[600],
+        fontFamily: "Montserrat"
+      )
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text("Click"),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
+));
