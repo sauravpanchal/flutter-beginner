9c2c75355a8803f913f2d862cd374655aa31edd2
Saurav Panchal <61927693+sauravpanchal@users.noreply.github.com>
2021-12-12 18:29:35 +0530

    Create v_11.dart

---
+++ v_11.dart
@@ -0,0 +1,56 @@
+// Rows Alignment
+
+
+// main.dart
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+//Stateless Widgets: State of widget cannot change over time.
+//Statefull WIdgets: State of widget can change over time. (Useful when data/state of widgets need to be updated.)
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Hello World !"),
+          TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.amber),
+            ),
+            child: Text(
+              "Click Me"
+            ),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text("Inside container it is !"),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("Click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
